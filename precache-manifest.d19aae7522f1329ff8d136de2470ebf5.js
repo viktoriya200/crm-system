60daf20f7a6c2b7422f2ffda5e4a0e2ab5941e24
Vika Marina <v.marina@idaproject.com>
2022-10-29 21:56:49 +0300

    deploy

---
+++ precache-manifest.d19aae7522f1329ff8d136de2470ebf5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11fce82118b942e09cd5",
+    "url": "/crm-system/css/app.07cf42cc.css"
+  },
+  {
+    "revision": "0bcc6a1a8b50ae2b6e1c",
+    "url": "/crm-system/css/chunk-4004e5c6.c8775c03.css"
+  },
+  {
+    "revision": "441c11cbbd3fd29ee885ae401f5b8ff6",
+    "url": "/crm-system/index.html"
+  },
+  {
+    "revision": "11fce82118b942e09cd5",
+    "url": "/crm-system/js/app.2ddfb14c.js"
+  },
+  {
+    "revision": "6033ae176bc3b443d712",
+    "url": "/crm-system/js/chunk-2d0a4783.d1a831cb.js"
+  },
+  {
+    "revision": "9f0bd384ec2249d40c1a",
+    "url": "/crm-system/js/chunk-2d0be70b.28ee546a.js"
+  },
+  {
+    "revision": "0bcc6a1a8b50ae2b6e1c",
+    "url": "/crm-system/js/chunk-4004e5c6.1750866e.js"
+  },
+  {
+    "revision": "470fb396601f79c2e71a",
+    "url": "/crm-system/js/chunk-vendors.495f644e.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/crm-system/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crm-system/robots.txt"
+  }
+]);
